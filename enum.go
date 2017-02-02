f3932efede09901976e816f8d01db0e075aee09f
Ernest Micklei <emicklei@Ernests-iMac-10.local>
2017-02-02 16:58:23 +0100

    reorder enum, fix group formatting, allow keyword as identify, remove unused ,prepare for proto2 group

--- enum.go
+++ enum.go
@@ -14,63 +14,12 @@ func (e *Enum) Accept(v Visitor) {
 	v.VisitEnum(e)
 }
 
-// EnumField is part of the body of an Enum.
-type EnumField struct {
-	Name        string
-	Integer     int
-	ValueOption *Option
-}
-
-// Accept dispatches the call to the visitor.
-func (f *EnumField) Accept(v Visitor) {
-	v.VisitEnumField(f)
-}
-
-// columns returns printable source tokens
-func (f EnumField) columns() (cols []aligned) {
-	cols = append(cols, leftAligned(f.Name), alignedEquals, rightAligned(strconv.Itoa(f.Integer)))
-	if f.ValueOption != nil {
-		cols = append(cols, f.ValueOption.columns()...)
-	}
-	return
-}
-
-func (f *EnumField) parse(p *Parser) error {
+func (e *Enum) parse(p *Parser) error {
 	tok, lit := p.scanIgnoreWhitespace()
 	if tok != tIDENT {
-		return p.unexpected(lit, "enum field identifier", f)
-	}
-	f.Name = lit
-	tok, lit = p.scanIgnoreWhitespace()
-	if tok != tEQUALS {
-		return p.unexpected(lit, "enum field =", f)
-	}
-	i, err := p.s.scanInteger()
-	if err != nil {
-		return p.unexpected(lit, "enum field integer", f)
-	}
-	f.Integer = i
-	tok, lit = p.scanIgnoreWhitespace()
-	if tok == tLEFTSQUARE {
-		o := new(Option)
-		o.IsEmbedded = true
-		err := o.parse(p)
-		if err != nil {
-			return err
+		if !isKeyword(tok) {
+			return p.unexpected(lit, "enum identifier", e)
 		}
-		f.ValueOption = o
-		tok, lit = p.scanIgnoreWhitespace()
-		if tok != tRIGHTSQUARE {
-			return p.unexpected(lit, "option closing ]", f)
-		}
-	}
-	return nil
-}
-
-func (e *Enum) parse(p *Parser) error {
-	tok, lit := p.s.scanIdent()
-	if tok != tIDENT {
-		return p.unexpected(lit, "enum identifier", e)
 	}
 	e.Name = lit
 	tok, lit = p.scanIgnoreWhitespace()
@@ -105,6 +54,61 @@ func (e *Enum) parse(p *Parser) error {
 done:
 	if tok != tRIGHTCURLY {
 		return p.unexpected(lit, "enum closing }", e)
+	}
+	return nil
+}
+
+// EnumField is part of the body of an Enum.
+type EnumField struct {
+	Name        string
+	Integer     int
+	ValueOption *Option
+}
+
+// Accept dispatches the call to the visitor.
+func (f *EnumField) Accept(v Visitor) {
+	v.VisitEnumField(f)
+}
+
+// columns returns printable source tokens
+func (f EnumField) columns() (cols []aligned) {
+	cols = append(cols, leftAligned(f.Name), alignedEquals, rightAligned(strconv.Itoa(f.Integer)))
+	if f.ValueOption != nil {
+		cols = append(cols, f.ValueOption.columns()...)
+	}
+	return
+}
+
+func (f *EnumField) parse(p *Parser) error {
+	tok, lit := p.scanIgnoreWhitespace()
+	if tok != tIDENT {
+		if !isKeyword(tok) {
+			return p.unexpected(lit, "enum field identifier", f)
+		}
+	}
+	f.Name = lit
+	tok, lit = p.scanIgnoreWhitespace()
+	if tok != tEQUALS {
+		return p.unexpected(lit, "enum field =", f)
+	}
+	i, err := p.s.scanInteger()
+	if err != nil {
+		return p.unexpected(lit, "enum field integer", f)
+	}
+	f.Integer = i
+	tok, lit = p.scanIgnoreWhitespace()
+	if tok == tLEFTSQUARE {
+		o := new(Option)
+		o.IsEmbedded = true
+		err := o.parse(p)
+		if err != nil {
+			return err
+		}
+		f.ValueOption = o
+		tok, lit = p.scanIgnoreWhitespace()
+		if tok != tRIGHTSQUARE {
+			return p.unexpected(lit, "option closing ]", f)
+		}
 	}
 	return nil
 }
